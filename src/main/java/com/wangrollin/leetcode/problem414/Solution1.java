5fc7e0ae99258ed21835cc0c8c8963efadadec77
wangrollin <wangrollin@foxmail.com>
2020-03-16 23:13:49 +0800

    第414题 第三大的数 - 稍稍优化

--- src/main/java/com/wangrollin/leetcode/problem414/Solution1.java
+++ src/main/java/com/wangrollin/leetcode/problem414/Solution1.java
@@ -36,47 +36,47 @@ public class Solution1 {
         int max2 = 0;
         int max3 = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int num : nums) {
             switch (maxCount) {
                 case 0:
-                    max1 = nums[i];
+                    max1 = num;
                     maxCount = 1;
                     break;
                 case 1:
-                    if (nums[i] > max1) {
+                    if (num > max1) {
                         max2 = max1;
-                        max1 = nums[i];
+                        max1 = num;
                         maxCount = 2;
-                    } else if (nums[i] < max1) {
-                        max2 = nums[i];
+                    } else if (num < max1) {
+                        max2 = num;
                         maxCount = 2;
                     }
                     break;
                 case 2:
-                    if (nums[i] > max1) {
+                    if (num > max1) {
                         max3 = max2;
                         max2 = max1;
-                        max1 = nums[i];
+                        max1 = num;
                         maxCount = 3;
-                    } else if (nums[i] < max1 && nums[i] > max2) {
+                    } else if (num < max1 && num > max2) {
                         max3 = max2;
-                        max2 = nums[i];
+                        max2 = num;
                         maxCount = 3;
-                    } else if (nums[i] < max2) {
-                        max3 = nums[i];
+                    } else if (num < max2) {
+                        max3 = num;
                         maxCount = 3;
                     }
                     break;
                 case 3:
-                    if (nums[i] > max1) {
+                    if (num > max1) {
                         max3 = max2;
                         max2 = max1;
-                        max1 = nums[i];
-                    } else if (nums[i] < max1 && nums[i] > max2) {
+                        max1 = num;
+                    } else if (num < max1 && num > max2) {
                         max3 = max2;
-                        max2 = nums[i];
-                    } else if (nums[i] < max2 && nums[i] > max3) {
-                        max3 = nums[i];
+                        max2 = num;
+                    } else if (num < max2 && num > max3) {
+                        max3 = num;
                     }
                     break;
             }

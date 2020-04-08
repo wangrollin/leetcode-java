08019bb166d7c986f2e2db825a3892c4649df782
wangrollin <wangrollin@foxmail.com>
2020-04-08 09:36:26 +0800

    第690题 员工的重要性

---
+++ src/main/java/com/wangrollin/leetcode/p600/p690/problem690/Employee.java
@@ -0,0 +1,14 @@
+package com.wangrollin.leetcode.p600.p690.problem690;
+
+import java.util.List;
+
+public class Employee {
+
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+}

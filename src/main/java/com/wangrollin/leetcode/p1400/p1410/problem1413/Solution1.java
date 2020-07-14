2d457e704e7c3828a45abaf8784d6c07c153314d
wangrollin <wangrollin@foxmail.com>
2020-07-14 09:50:53 +0800

    第1413题 逐步求和得到正数的最小值

---
+++ src/main/java/com/wangrollin/leetcode/p1400/p1410/problem1413/Solution1.java
@@ -0,0 +1,55 @@
+package com.wangrollin.leetcode.p1400.p1410.problem1413;
+
+/**
+ * 逐步求和得到正数的最小值
+ *
+ * 给你一个整数数组 nums 。你可以选定任意的 正数 startValue 作为初始值。
+ * 你需要从左到右遍历 nums 数组，并将 startValue 依次累加上 nums 数组中的值。
+ * 请你在确保累加和始终大于等于 1 的前提下，选出一个最小的 正数 作为 startValue 。
+ *
+ * 示例 1：
+ * 输入：nums = [-3,2,-3,4,2]
+ * 输出：5
+ * 解释：如果你选择 startValue = 4，在第三次累加时，和小于 1 。
+ *                 累加求和
+ *                 startValue = 4 | startValue = 5 | nums
+ *                   (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+ *                   (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+ *                   (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+ *                   (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+ *                   (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+ *
+ * 示例 2：
+ * 输入：nums = [1,2]
+ * 输出：1
+ * 解释：最小的 startValue 需要是正数。
+ *
+ * 示例 3：
+ * 输入：nums = [1,-2,-3]
+ * 输出：5
+ *
+ * 提示：
+ * 1 <= nums.length <= 100
+ * -100 <= nums[i] <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-value-to-get-positive-step-by-step-sum
+ *
+ * Solution1
+ * 在一个一个数加的时候，始终维护一个最小的值，然后和1的距离就是答案
+ */
+public class Solution1 {
+
+    public int minStartValue(int[] nums) {
+
+        int minSum = 0;
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+            minSum = Math.min(minSum, sum);
+        }
+
+        return Math.max(1, 1 - minSum);
+    }
+}

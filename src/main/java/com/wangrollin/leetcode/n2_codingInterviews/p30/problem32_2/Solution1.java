b4eaa211a03556f11827eefacb88d08d0b286f83
wangrollin <wangrollin@foxmail.com>
2020-10-02 13:35:47 +0800

    剑指Offer 32_2题 从上到下打印二叉树 II

---
+++ src/main/java/com/wangrollin/leetcode/n2_codingInterviews/p30/problem32_2/Solution1.java
@@ -0,0 +1,86 @@
+package com.wangrollin.leetcode.n2_codingInterviews.p30.problem32_2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+/**
+ * 从上到下打印二叉树 II
+ *
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ *
+ * 例如:
+ * 给定二叉树:[3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ * 
+ * 提示：
+ * 节点总数 <= 1000
+ *
+ * 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+ *
+ * Solution1
+ * 层序遍历
+ */
+public class Solution1 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            result.add(queue
+                    .stream()
+                    .mapToInt(treeNode -> treeNode.val)
+                    .boxed()
+                    .collect(Collectors.toList()));
+
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                size--;
+            }
+        }
+
+        return result;
+    }
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}

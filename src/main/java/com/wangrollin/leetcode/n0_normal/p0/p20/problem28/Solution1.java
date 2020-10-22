413f5251238c312b5af8e99c082a38ce22594885
wangrollin <wangrollin@foxmail.com>
2020-10-22 10:05:16 +0800

    第28题 实现 strStr()

---
+++ src/main/java/com/wangrollin/leetcode/n0_normal/p0/p20/problem28/Solution1.java
@@ -0,0 +1,58 @@
+package com.wangrollin.leetcode.n0_normal.p0.p20.problem28;
+
+/**
+ * 实现 strStr()
+ *
+ * 实现strStr()函数。
+ * 给定一个haystack 字符串和一个 needle 字符串，
+ * 在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回 -1。
+ *
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ *
+ * 说明:
+ * 当needle是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 对于本题而言，当needle是空字符串时我们应当返回 0 。这与C语言的strstr()以及 Java的indexOf()定义相符。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ *
+ * Solution1
+ * 遍历对比
+ */
+public class Solution1 {
+
+    public int strStr(String haystack, String needle) {
+
+        if (needle == null || "".equals(needle)) {
+            return 0;
+        }
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            if (checkTheRest(haystack, needle, i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private boolean checkTheRest(String haystack, String needle, int startIndex) {
+
+        for (int i = 0; i < needle.length(); i++, startIndex++) {
+            if (haystack.charAt(startIndex) != needle.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

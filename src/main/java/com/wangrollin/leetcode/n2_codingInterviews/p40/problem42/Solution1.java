392bef253f892f525d7ab4ee489b4bb98ba5728c
wangrollin <wangrollin@foxmail.com>
2020-10-27 09:46:22 +0800

    完善代码

--- src/main/java/com/wangrollin/leetcode/n2_codingInterviews/p40/problem42/Solution1.java
+++ src/main/java/com/wangrollin/leetcode/n2_codingInterviews/p40/problem42/Solution1.java
@@ -1,20 +1,24 @@
 package com.wangrollin.leetcode.n2_codingInterviews.p40.problem42;
 
 /**
- * 最大子序和
+ * 连续子数组的最大和
  *
- * 给定一个整数数组 nums，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * 要求时间复杂度为O(n)。
  *
- * 示例:
- * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 示例1:
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
  * 输出: 6
  * 解释:连续子数组[4,-1,2,1] 的和最大，为6。
  *
- * 进阶:
- * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * 提示：
+ * 1 <=arr.length <= 10^5
+ * -100 <= arr[i] <= 100
+ *
+ * 注意：本题与主站 53 题相同：https://leetcode-cn.com/problems/maximum-subarray/
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof
  *
  * Solution1
  * 动态规划，迭代，状态转移方程 以下标n结尾的最大和：f(n) = max{arr[n] + f(n-1), arr[n]}

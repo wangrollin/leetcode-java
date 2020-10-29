9a0b9276e226b6a5e78888c279010a80a1a6ce57
wangrollin <wangrollin@foxmail.com>
2020-10-29 09:44:36 +0800

    第121题 买卖股票的最佳时机

---
+++ src/main/java/com/wangrollin/leetcode/n0_normal/p100/p120/problem121/Solution1.java
@@ -0,0 +1,47 @@
+package com.wangrollin.leetcode.n0_normal.p100.p120.problem121;
+
+/**
+ * 买卖股票的最佳时机
+ *
+ * 给定一个数组，它的第i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意：你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ *
+ * 示例 2:
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ *
+ * Solution1
+ * 迭代，存储前一次的数据
+ * f(i) 代表第i天卖出的情况下，第一天到第i天的最大利润
+ * 状态转移方程：f(n) = max(0, f(n-1) + arr[n] - arr[n-1])
+ */
+public class Solution1 {
+
+    public int maxProfit(int[] prices) {
+
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int pre = Math.max(prices[1] - prices[0], 0);
+        int max = pre;
+
+        for (int i = 2; i < prices.length; i++) {
+            pre = Math.max(0, pre + prices[i] - prices[i - 1]);
+            max = Math.max(pre, max);
+        }
+        return max;
+    }
+}

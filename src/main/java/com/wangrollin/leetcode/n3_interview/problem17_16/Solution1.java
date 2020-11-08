53a701e4aec1adfd20deef5b39056bfc9a4cc3eb
wangrollin <wangrollin@foxmail.com>
2020-11-08 14:53:24 +0800

    面试题17.16 按摩师

---
+++ src/main/java/com/wangrollin/leetcode/n3_interview/problem17_16/Solution1.java
@@ -0,0 +1,66 @@
+package com.wangrollin.leetcode.n3_interview.problem17_16;
+
+/**
+ * 按摩师
+ *
+ * 一个有名的按摩师会收到源源不断的预约请求，每个预约都可以选择接或不接。
+ * 在每次预约服务之间要有休息时间，因此她不能接受相邻的预约。
+ * 给定一个预约请求序列，替按摩师找到最优的预约集合（总预约时间最长），返回总的分钟数。
+ *
+ * 注意：本题相对原题稍作改动
+ *
+ * 示例 1：
+ * 输入： [1,2,3,1]
+ * 输出： 4
+ * 解释： 选择 1 号预约和 3 号预约，总时长 = 1 + 3 = 4。
+ *
+ * 示例 2：
+ * 输入： [2,7,9,3,1]
+ * 输出： 12
+ * 解释： 选择 1 号预约、 3 号预约和 5 号预约，总时长 = 2 + 9 + 1 = 12。
+ *
+ * 示例 3：
+ * 输入： [2,1,4,5,3,1,1,3]
+ * 输出： 12
+ * 解释： 选择 1 号预约、 3 号预约、 5 号预约和 8 号预约，总时长 = 2 + 4 + 3 + 3 = 12。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/the-masseuse-lcci
+ * 
+ * Solution1
+ * 存储三次结果，迭代
+ * f(i)代表在第i位被选择的情况下，从开始到第i位最大的时长总和
+ * f(i) = max(f(i-2), f(i-3)) + arr_i
+ */
+public class Solution1 {
+
+    public int massage(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        if (nums.length == 3) {
+            return Math.max(nums[0] + nums[2], nums[1]);
+        }
+
+        int first = nums[0];
+        int second = nums[1];
+        int third = nums[0] + nums[2];
+
+        for (int i = 3; i < nums.length; i++) {
+
+            int temp = Math.max(first, second) + nums[i];
+            first = second;
+            second = third;
+            third = temp;
+        }
+
+        return Math.max(second, third);
+    }
+}

aa03cd8e89a9bcb1bde4522ad4aeb2e0f1da9e68
wangrollin <wangrollin@foxmail.com>
2021-03-15 10:07:23 +0800

    第1716题 计算力扣银行的钱

---
+++ src/main/java/com/wangrollin/leetcode/n0_normal/p1700/p1710/problem1716/Solution1.java
@@ -0,0 +1,51 @@
+package com.wangrollin.leetcode.n0_normal.p1700.p1710.problem1716;
+
+/**
+ * 计算力扣银行的钱
+ *
+ * Hercy 想要为购买第一辆车存钱。他 每天 都往力扣银行里存钱。
+ * 最开始，他在周一的时候存入 1块钱。从周二到周日，他每天都比前一天多存入 1块钱。
+ * 在接下来每一个周一，他都会比 前一个周一 多存入 1块钱。
+ * 给你n，请你返回在第 n天结束的时候他在力扣银行总共存了多少块钱。
+ *
+ * 示例 1：
+ * 输入：n = 4
+ * 输出：10
+ * 解释：第 4 天后，总额为 1 + 2 + 3 + 4 = 10 。
+ *
+ * 示例 2：
+ * 输入：n = 10
+ * 输出：37
+ * 解释：第 10 天后，总额为 (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37 。注意到第二个星期一，Hercy 存入 2 块钱。
+ *
+ * 示例 3：
+ * 输入：n = 20
+ * 输出：96
+ * 解释：第 20 天后，总额为 (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96 。
+ * 
+ * 提示：
+ * 1 <= n <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank
+ *
+ * Solution1
+ * 数学方法计算
+ */
+public class Solution1 {
+
+    public int totalMoney(int n) {
+
+        int weekNum = n / 7;
+        int dayNum = n % 7;
+        int basis = 28;
+
+        int sum1 = weekNum * basis;
+        int sum2 = 7 * (weekNum - 1) * weekNum / 2;
+
+        int lastMon = weekNum + 1;
+        int lastWeekSum = (dayNum - 1) * dayNum / 2 + dayNum * lastMon;
+
+        return sum1 + sum2 + lastWeekSum;
+    }
+}

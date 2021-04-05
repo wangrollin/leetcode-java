dac3340caa48ef759c51d46bec0e257b34d5c44e
wangrollin <wangrollin@foxmail.com>
2021-04-05 22:34:12 +0800

    第199题 二叉树的右视图

---
+++ src/main/java/com/wangrollin/leetcode/n0_normal/p100/p190/problem199/Solution1.java
@@ -0,0 +1,75 @@
+package com.wangrollin.leetcode.n0_normal.p100.p190.problem199;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的右视图
+ *
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ * 示例:
+ * 输入:[1,2,3,null,5,null,4]
+ * 输出:[1, 3, 4]
+ * 解释:
+ *    1            <---
+ *  /   \
+ * 2     3         <---
+ *  \     \
+ *   5     4       <---
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+ *
+ * Solution1
+ * 层序遍历
+ * dfs也可以，中->右->左 遍历，如果某一深度的记录为空，则可以占据
+ */
+public class Solution1 {
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (queue.size() != 0) {
+
+            int size = queue.size();
+
+            while (size != 0) {
+                TreeNode node = queue.poll();
+                if (size == 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                size--;
+            }
+        }
+
+        return result;
+    }
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}

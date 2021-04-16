8d0d4353477f7d70eeadc3482ebe6a3c26921587
wangrollin <wangrollin@foxmail.com>
2021-04-16 23:36:55 +0800

    第1773题 统计匹配检索规则的物品数量

---
+++ src/main/java/com/wangrollin/leetcode/n0_normal/p1700/p1770/problem1773/Solution1.java
@@ -0,0 +1,62 @@
+package com.wangrollin.leetcode.n0_normal.p1700.p1770.problem1773;
+
+import java.util.List;
+
+/**
+ * 统计匹配检索规则的物品数量
+ *
+ * 给你一个数组 items ，其中items[i] = [typei, colori, namei] ，描述第 i 件物品的类型、颜色以及名称。
+ * 另给你一条由两个字符串ruleKey 和 ruleValue 表示的检索规则。
+ * 如果第 i 件物品能满足下述条件之一，则认为该物品与给定的检索规则 匹配 ：
+ * ruleKey == "type" 且 ruleValue == typei 。
+ * ruleKey == "color" 且 ruleValue == colori 。
+ * ruleKey == "name" 且 ruleValue == namei 。
+ * 统计并返回 匹配检索规则的物品数量 。
+ *
+ * 示例 1：
+ * 输入：items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+ * 输出：1
+ * 解释：只有一件物品匹配检索规则，这件物品是 ["computer","silver","lenovo"] 。
+ *
+ * 示例 2：
+ * 输入：items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+ * 输出：2
+ * 解释：只有两件物品匹配检索规则，这两件物品分别是 ["phone","blue","pixel"] 和 ["phone","gold","iphone"] 。
+ * 注意，["computer","silver","phone"] 未匹配检索规则。
+ *
+ * 提示：
+ * 1 <= items.length <= 104
+ * 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ * ruleKey 等于 "type"、"color" 或 "name"
+ * 所有字符串仅由小写字母组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-items-matching-a-rule
+ *
+ * Solution1
+ * stream计算
+ */
+public class Solution1 {
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int index = 0;
+        switch (ruleKey) {
+            case "type":
+                index = 0;
+                break;
+            case "color":
+                index = 1;
+                break;
+            case "name":
+                index = 2;
+                break;
+        }
+
+        int finalIndex = index;
+        return (int) items.stream()
+                .map(list -> list.get(finalIndex))
+                .filter(value -> ruleValue.equals(value))
+                .count();
+    }
+}
